82da1dd071713268a4dc02851ebaf2e03d1a914b
xylonx <xylonx@dingtalk.com>
2021-09-16 13:41:37 +0800

    :tada: init repo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"Header":  ctx.Request.Header,
+			"message": "pong",
+		})
+	})
+
+	r.Run(":5000")
+}
